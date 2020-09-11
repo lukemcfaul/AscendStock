a2ed6e0195382e8dc82224ea15630019b4e54bbc
lukemcfaul <66988084+lukemcfaul@users.noreply.github.com>
2020-09-10 20:00:11 -0700

    Update ATCNetworkingManager.swift

--- xcode/Core/Networking/ATCNetworkingManager.swift
+++ xcode/Core/Networking/ATCNetworkingManager.swift
@@ -1,10 +1,3 @@
-//
-//  ATCNetworkingManager.swift
-//  AppTemplatesCore
-//
-//  Created by Florian Marcu on 2/2/17.
-//  Copyright © 2017 iOS App Templates. All rights reserved.
-//
 
 import Alamofire
 
